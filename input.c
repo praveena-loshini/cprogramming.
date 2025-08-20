c4d1d85920f14d80fc5e206c90e27acd3ccbc4cc
praveena-loshini <praveenaloshini001@gmail.com>
2025-08-20 04:50:20 +0000

    Input.c

---
+++ input.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int age;
+    char name[30];
+    scanf("%d%s",&age,&name);
+    printf("%d\n%s",age,name);
+}//Write a C program to obtain and display the newly joined student's name and age detail. Input Format: The First line of input consists of a string that corresponds to the name of the student. The second line of input consists of an integer that corresponds to the age of the student. Output Format: The output prints the name and age details of the student. 
