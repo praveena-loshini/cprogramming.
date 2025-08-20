79dd2685fdfbff81562a17f4a79e7466608e0e24
praveena-loshini <praveenaloshini001@gmail.com>
2025-08-20 04:35:12 +0000

    code

---
+++ newspapercost.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int copies,costpercopy,spends,profit;
+    scanf("%d%d%d",&copies,&costpercopy,&spends);
+    profit=(copies*costpercopy-copies*spends)-100;
+     printf("%d",profit);
+}//Each Saturday, The Herald sells 'a' copies of a special edition newspaper for Rs. b per copy. The cost to him for printing each newspaper is Rs. c. He pays a fixed cost of Rs.100 per Saturday for storage, delivery, and so on. He wants to calculate the profit which it obtains only on Saturdays. Can you please help him out by writing a program to compute the profit if a, b, and c are given? Input Format: Input consists of 3 integers: a, b, and c. a is the number of copies sold, b is the cost per copy and c is the cost The Herald spends per copy. Output Format: The output consists of a single integer which corresponds to the profit obtained by The Herald. Each Saturday, The Herald sells 'a' copies of a special edition newspaper for Rs. b per copy. The cost to him for printing each newspaper is Rs. c. He pays a fixed cost of Rs.100 per Saturday for storage, delivery, and so on. He wants to calculate the profit which it obtains only on Saturdays. Can you please help him out by writing a program to compute the profit if a, b, and c are given? Input Format: Input consists of 3 integers: a, b, and c. a is the number of copies sold, b is the cost per copy and c is the cost The Herald spends per copy. Output Format: The output consists of a single integer which corresponds to the profit obtained by The Herald. 
