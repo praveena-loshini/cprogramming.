cdabdd5737333a9afb33543609908f3b5b7d5cb3
praveena-loshini <praveenaloshini001@gmail.com>
2025-09-29 21:36:00 +0530

    Create descendingarray.c

---
+++ descendingarray.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+ int a[3];
+ for(int i=0;i<3;i++){
+     scanf("%d",&a[i]);
+ }
+ for(int i=0;i<3;i++){
+ for(int j=i+1;j<3;j++){
+     if(a[i]<a[j]){
+         int temp=a[i];
+         a[i]=a[j];
+         a[j]=temp;
+         
+     }}
+ }
+ for(int i=0;i<3;i++){
+     printf("%d ",a[i]);
+ }
+ 
+}
